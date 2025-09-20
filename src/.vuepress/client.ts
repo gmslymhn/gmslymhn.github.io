4411fa338d999d094346a2e55e8b38abcb07ae52
gmslymhn <gmslymhn@gmail.com>
2025-09-20 18:47:32 +0800

    测试

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,7 +1,19 @@
 import { defineClientConfig } from "vuepress/client";
 import Blog from "./layouts/Blog.vue";
+// import PdfPreview from "./PdfPreview/index.vue";
+// 只在客户端导入和注册组件
+let PdfPreview: any = null
 export default defineClientConfig({
     layouts: {
         Blog, // 覆盖默认的 Blog 布局
-    }
+    },
+    enhance({ app }) {
+        // 只在客户端环境下注册组件
+        if (typeof window !== 'undefined') {
+            import('./PdfPreview/index.vue').then((module) => {
+                PdfPreview = module.default
+                app.component('PdfPreview', PdfPreview)
+            })
+        }
+    },
 });
